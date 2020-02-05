cc27cd2573204ed99252581e4524ccd038836d08
prabhu-raja <prabhuraja@gmail.com>
2020-02-05 07:19:37 -0500

    Inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log('Hey there');
+class Sizes {
+  constructor(private sizes: string[]) { }
+  
+  set avialbleSizes(val : string[]) {
+    this.sizes = val;
+  }
+
+  get availableSizes() : string[] {
+    return this.sizes;
+  }
+  
+}
+
+class Pizza extends Sizes {
+  toppings: string[] = [];
+  constructor(
+    readonly name: string,
+    sizes: string[]) {
+      super(sizes);
+  }
+
+  addToppings(val: string) {
+    this.toppings.push(val);
+  }
+}
+
+const pizza = new Pizza('Veggie', ['sm', 'md']);
+pizza.addToppings('Black Olive');
+console.log(pizza);
